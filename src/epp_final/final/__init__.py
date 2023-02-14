58fa981bef63f65b820296e152ed273aba59de28
yudingshechu <geweicao@126.com>
2023-02-14 15:11:58 +0100

    fix directory figure loss bug

--- src/epp_final/final/__init__.py
+++ src/epp_final/final/__init__.py
@@ -1,5 +1,5 @@
 """Functions for formatting results."""
 
-from epp_final.final.plot import reformat_data
+from epp_final.final.plot import plot_results, plot_results_regional, reformat_data
 
-__all__ = [reformat_data]
+__all__ = [reformat_data, plot_results, plot_results_regional]
--- src/epp_final/final/task_final.py
+++ src/epp_final/final/task_final.py
@@ -8,10 +8,12 @@ from epp_final.config import BLD
 from epp_final.final.plot import plot_results, plot_results_regional, reformat_data
 
 kwargs = {
-    "produces1": BLD / "python" / "figures" / "A3.png",
-    "produces2": BLD / "python" / "figures" / "PESR.png",
-    "produces3": BLD / "python" / "figures" / "A3_regional.png",
-    "produces4": BLD / "python" / "figures" / "PESR_regional.png",
+    "produces": {
+        "produces1": BLD / "python" / "figures" / "A3.png",
+        "produces2": BLD / "python" / "figures" / "PESR.png",
+        "produces3": BLD / "python" / "figures" / "A3_regional.png",
+        "produces4": BLD / "python" / "figures" / "PESR_regional.png",
+    },
 }
 
 
@@ -23,7 +25,7 @@ kwargs = {
     },
 )
 @pytask.mark.task(kwargs=kwargs)
-def task_plot_results_all(depends_on, produces1, produces2, produces3, produces4):
+def task_plot_results_all(depends_on, produces):
     """Plot the regression results by age (Python version)."""
     data = load_model(depends_on["data"])
     a3_all, Pesr_all = reformat_data(data)
@@ -40,8 +42,8 @@ def task_plot_results_all(depends_on, produces1, produces2, produces3, produces4
         {"x": "Year", "y": "PESR"},
         "Policy Effect on Sex Ratio",
     )
-    fig1.write_image(produces1)
-    fig2.write_image(produces2)
+    fig1.write_image(produces["produces1"])
+    fig2.write_image(produces["produces2"])
     dfa3_regional = load_model(depends_on["dfa3_regional"])
     dfpesr_regional = load_model(depends_on["dfpesr_regional"])
     fig3 = plot_results_regional(
@@ -54,5 +56,5 @@ def task_plot_results_all(depends_on, produces1, produces2, produces3, produces4
         {"x": "Year", "y": "PESR"},
         "Policy Effect on Sex Ratio",
     )
-    fig3.write_image(produces3)
-    fig4.write_image(produces4)
+    fig3.write_image(produces["produces3"])
+    fig4.write_image(produces["produces4"])
